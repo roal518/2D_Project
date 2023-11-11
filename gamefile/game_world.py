cac542c7955a592986113bc228f4abf151c4c4df
roal518 <roalqwerty@gmail.com>
2023-11-11 20:54:39 +0900

    프레임워크로 변경

--- gamefile/game_world.py
+++ gamefile/game_world.py
@@ -1,23 +1,85 @@
-objects = [[],[]]
-#객체별 계층 생성? 어려운데???
-def add_object(o , depth = 0):
+objects = [[] for _ in range(4)]  # 보이는 세계
+
+# fill here
+# 충돌의 세계
+collision_pairs = {}  # 'boy:ball': [ [boy], [ball1, ball2...]]
+
+
+def add_object(o, depth=0):
     objects[depth].append(o)
-def add_objects(ol,depth = 0):
+
+
+def add_objects(ol, depth=0):
     objects[depth] += ol
+
 
 def update():
     for layer in objects:
         for o in layer:
             o.update()
+
+
 def render():
     for layer in objects:
         for o in layer:
             o.draw()
 
+
+# fill here
+def add_collision_pair(group, a=None, b=None):  # a 와 b 사이에 충돌 검사가 필요하다는 점을 등록
+    if group not in collision_pairs:
+        print(f'NEW group {group} added......')
+        collision_pairs[group] = [[], []]
+    if a:
+        collision_pairs[group][0].append(a)
+    if b:
+        collision_pairs[group][1].append(b)
+
+    pass
+
+
+def remove_collision_object(o):
+    for pairs in collision_pairs.values():
+        if o in pairs[0]:
+            pairs[0].remove(o)
+        if o in pairs[1]:
+            pairs[1].remove(o)
+    pass
+
+
 def remove_object(o):
     for layer in objects:
         if o in layer:
             layer.remove(o)
+            remove_collision_object(o)
+            del o
             return
-        #더 지울 대상은 없기에 return
-    raise ValueError('cannot remove non-existing obj')
+    raise ValueError('Cannot delete non existing object')
+
+
+def clear():
+    for layer in objects:
+        layer.clear()
+
+
+# fill here
+
+
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True
+
+
+def handle_collision():
+    for group, pairs in collision_pairs.items():
+        for a in pairs[0]:
+            for b in pairs[1]:
+                if collide(a, b):
+                    a.handle_collision(group, b)
+                    b.handle_collision(group, a)
---
+++ gamefile/play_mode.py
@@ -0,0 +1,63 @@
+import random
+
+from pico2d import *
+import game_framework
+
+import game_world
+from field import *
+from player import *
+from ball import Ball
+
+# boy = None
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            boy.handle_event(event)
+
+def init():
+    global grass
+    global boy
+
+    running = True
+
+    grass = Grass()
+    game_world.add_object(grass, 0)
+
+    boy = Boy()
+    game_world.add_object(boy, 1)
+
+    # fill here
+    global balls
+    balls = [Ball(random.randint(0,1600),60,0) for _ in range(50)]
+    game_world.add_objects(balls, 1)
+
+    game_world.add_collision_pair('boy.ball', boy, None) #소년을 등록
+    for ball in balls:
+        game_world.add_collision_pair('boy:ball', boy, ball)
+
+def finish():
+    game_world.clear()
+    pass
+
+
+def update():
+    game_world.update()
+
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def pause():
+    pass
+
+def resume():
+    pass
+
