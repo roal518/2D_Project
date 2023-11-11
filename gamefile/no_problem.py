89b40fa15d34f9270cb50f4f8202e4c093a5837e
roal518 <roalqwerty@gmail.com>
2023-11-11 20:53:41 +0900

    Delete no_problem.py

--- gamefile/no_problem.py
+++
@@ -1,8 +0,0 @@
-from pico2d import *
-
-class Player:
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
-        self.speed = 5
-        self.image = load_image('animation_sheet.png')
