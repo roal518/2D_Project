1ebf787d61e00aacc3beb5c8061801375f0c2f8b
roal518 <roalqwerty@gmail.com>
2023-11-28 02:18:43 +0900

    rename/update play_multi_mode.py

--- gamefile/main.py
+++ gamefile/main.py
@@ -1,7 +1,7 @@
 from pico2d import open_canvas, delay, close_canvas
 import game_framework
 
-import play_mode as start_mode
+import play_multi_mode as start_mode
 
 open_canvas(1600, 600)
 game_framework.run(start_mode)
